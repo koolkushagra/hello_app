c73b0fa5b5fb1cc4464f89a274a0945303a8e5d1
Kushagra Vaish <vaish.kushagra@gmail.com>
2015-12-07 08:18:44 +0000

    Static Pages Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
+  
   resources :microposts
   resources :users
-  root 'application#hello'
+  
+  root 'static_pages#home'
   
 end
