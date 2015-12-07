66b2b70347fa7b5266809b5ee3c72216bb5042de
Kushagra Vaish <vaish.kushagra@gmail.com>
2015-12-07 09:07:35 +0000

    Added Some default functionailty

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def full_title(page_title = '')
+    base_title = "RoR Tut"
+    if page_title == ''
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
 end
